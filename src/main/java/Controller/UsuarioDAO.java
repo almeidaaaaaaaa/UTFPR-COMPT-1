3b657a2db2043b5d3c56f5dbb2b2a521ab6a6dc2
Gustavo <gutyrocha1@hotmail.com>
2025-04-29 14:30:27 -0300

    conexao com bd

---
+++ src/main/java/Controller/UsuarioDAO.java
@@ -0,0 +1,35 @@
+
+package Controller;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+
+public class UsuarioDAO {
+    
+    static Connection conn = null;
+    static String url = "jdbc:mysql://localhost:3306/competente";
+    static String driver = "com.mysql.cj.jdbc.Driver";
+    static String user = "root";
+    static String senha = "2004Gu$tavo";
+    
+    public static void main(String[] args){ //teste para conexão com bd
+        try{
+            System.out.println("Carregando o driver...");
+            Class.forName(driver);
+            System.out.println("Driver carregado com sucesso!");
+        }catch(Exception e){
+            System.out.println("Falha no carregamento!");
+    }
+
+        try{
+            System.out.println("Tentando conectar o BD...");
+            conn = DriverManager.getConnection(url,user,senha);
+            System.out.println("BD conectado com sucesso!");
+        }catch(Exception e){
+            System.out.println("Falha no carregamento!");
+        }
+    }
+}
+
+
