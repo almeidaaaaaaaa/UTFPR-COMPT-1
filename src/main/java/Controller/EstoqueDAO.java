d4ff5bb92363b9ad1baff40ff51ce7841b2bd448
Gustavo <gutyrocha1@hotmail.com>
2025-06-10 18:55:38 -0300

    Adição da lógica do estoque e seus respectivos testes

--- src/main/java/Controller/EstoqueDAO.java
+++ src/main/java/Controller/EstoqueDAO.java
@@ -1,13 +1,79 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package Controller;
 
-/**
- *
- * @author joaom
- */
-public class EstoqueDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import Model.Estoque;
+import java.sql.Timestamp;
+
+public class EstoqueDAO{
     
+        public static void inserir(Estoque s) throws SQLException {
+        ConexaoBD bd = new ConexaoBD();
+        try (Connection conn = bd.getConnection()) {
+            if (conn != null) {
+                String sql = "INSERT INTO estoque(Est_cod, Est_dataE, Est_dataS, Est_destino, Voluntario_Vol_cod, Voluntario_Usuario_Usu_rg) VALUES (?, ?, ?, ?, ?, ?)";
+                try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                    stmt.setInt(1, s.getCodigo());
+                    stmt.setTimestamp(2, Timestamp.valueOf(s.getDataE2()));
+                    stmt.setTimestamp(3, Timestamp.valueOf(s.getDataS()));
+                    stmt.setString(4, s.getDestino());
+                    stmt.setInt(5, s.getCod());
+                    stmt.setInt(6, s.getRG());
+                    stmt.executeUpdate();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                    throw new RuntimeException("Não foi possivel inserir novo estoque", e);
+                }
+            } else {
+                throw new RuntimeException("Nao foi possivel conectar ao banco.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Nao foi possivel conectar ao banco", e);
+        }
+    }
+        
+        public static void Excluir(int cod) throws SQLException {
+        ConexaoBD bd = new ConexaoBD();
+        try (Connection conn = bd.getConnection()) {
+            if (conn != null) {
+                String sql = "DELETE FROM estoque WHERE Est_cod = ?";
+                try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                    stmt.setInt(1, cod);
+                    
+                    stmt.executeUpdate();
+                } catch (SQLException e) {
+                    throw new RuntimeException("Não foi possivel excluir estoque", e);
+                }
+            } else {
+                throw new RuntimeException("Nao foi possivel conectar ao banco.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Nao foi possivel conectar ao banco", e);
+        }
+    }
+        
+        public static void Atualizar(Estoque s) throws SQLException {
+        ConexaoBD bd = new ConexaoBD();
+        try (Connection conn = bd.getConnection()) {
+            if (conn != null) {
+                String sql = "UPDATE estoque SET Est_dataE = ?, Est_dataS = ?, Est_destino = ? WHERE Est_cod = ?";
+                try (PreparedStatement stmt = conn.prepareStatement(sql)) {
+                    stmt.setTimestamp(1, Timestamp.valueOf(s.getDataE2()));
+                    stmt.setTimestamp(2, Timestamp.valueOf(s.getDataS()));
+                    stmt.setString(3, s.getDestino());
+                    stmt.setInt(4, s.getCodigo());
+             
+                    stmt.executeUpdate();
+                } catch (SQLException e) {
+                    throw new RuntimeException("Não foi possivel atualizar estoque", e);
+                }
+            } else {
+                throw new RuntimeException("Nao foi possivel conectar ao banco.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Nao foi possivel conectar ao banco", e);
+        }
+    }
 }
--- src/main/java/Model/Estoque.java
+++ src/main/java/Model/Estoque.java
@@ -1,21 +1,21 @@
 package Model;
 
 import java.util.List;
+import java.time.LocalDateTime;
 
-public class Estoque {
+public class Estoque extends Voluntario{
     
     private int codigo;
-    private int dataE;
-    private int dataS;
-    private String doador;
+    private LocalDateTime dataE2;
+    private LocalDateTime dataS;
     private String destino;
     private final List<Produto> produtos;
 
-    public Estoque(int codigo, int dataE, int dataS, String doador, String destino, List<Produto> produtos) {
+    public Estoque(int codigo, LocalDateTime dataE2, LocalDateTime dataS, String destino, List<Produto> produtos, int cod, LocalDateTime dataE, String nome, int RG, int cargo, String email, String senha) {
+        super(cod,  dataE, nome, RG, cargo, email, senha);
         this.codigo = codigo;
-        this.dataE = dataE;
+        this.dataE2 = dataE2;
         this.dataS = dataS;
-        this.doador = doador;
         this.destino = destino;
         this.produtos = produtos;
     }
@@ -48,27 +48,30 @@ public class Estoque {
         this.codigo = codigo;
     }
 
-    public int getDataE() {
-        return dataE;
+
+    /**
+     * @return the dataE
+     */
+    public LocalDateTime getDataE2() {
+        return dataE2;
     }
 
-    public void setDataE(int dataE) {
-        this.dataE = dataE;
+  
+    public void setDataE2(LocalDateTime dataE2) {
+        this.dataE2 = dataE2;
     }
 
-    public int getDataS() {
+    /**
+     * @return the dataS
+     */
+    public LocalDateTime getDataS() {
         return dataS;
     }
 
-    public void setDataS(int dataS) {
+    /**
+     * @param dataS the dataS to set
+     */
+    public void setDataS(LocalDateTime dataS) {
         this.dataS = dataS;
-    }
-
-    public String getDoador() {
-        return doador;
-    }
-
-    public void setDoador(String doador) {
-        this.doador = doador;
     }
 }
